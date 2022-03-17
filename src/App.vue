58ee937354f89de1d37e827de0a0d6751c24153f
dymafr <erwan1309@hotmail.com>
2022-03-17 13:39:02 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
   <form @submit="submit">
     <div>
-      <select v-model="cityValue">
-        <option value disabled>--Choisissez une ville--</option>
-        <option value="Nice">Nice</option>
-        <option value="Paris">Paris</option>
-        <option value="Lyon">Lyon</option>
-      </select>
+      <label>Activités</label>
+      <button type="button">Ajouter une activité</button>
     </div>
-    <pre>{{ errorMessage }}</pre>
-    <div>
-      <input v-model="zipValue" type="number" />
-    </div>
-    <div>
-      <input v-model="minValue" type="number" />
-    </div>
-    <div>
-      <input v-model="maxValue" type="number" />
-    </div>
-    <pre>{{ values }}</pre>
     <button>Envoi</button>
   </form>
 </template>
 
 <script setup lang="ts">
-import { useField, useForm } from 'vee-validate';
+import { useFieldArray, useForm } from 'vee-validate';
 import { z } from 'zod';
 import { toFieldValidator } from '@vee-validate/zod';
 
@@ -32,17 +17,7 @@ const { handleSubmit, values } = useForm();
 
 const submit = handleSubmit(() => {});
 
-const { value: cityValue, errorMessage } = useField(
-  'address.city',
-  toFieldValidator(z.string({ required_error: 'Veuillez choisir une ville' }), {
-    validateOnValueUpdate: false,
-    initialValue: '',
-  })
-);
-const { value: zipValue } = useField('address.zip.code');
-
-const { value: minValue } = useField('minMax[0]');
-const { value: maxValue } = useField('minMax[1]');
+const { value: cityValue, errorMessage } = useFieldArray('address.city');
 </script>
 
 <style scoped lang="scss"></style>
